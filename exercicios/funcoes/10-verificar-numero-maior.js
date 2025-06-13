e2446e305af8355e987c6535499a0a870dd096d8
laymel0 <antonia_melo_pereira@hotmail.com>
2025-06-12 23:00:17 -0300

    Respostas dos exercicios 9 e 10

--- exercicios/funcoes/10-verificar-numero-maior.js
+++ exercicios/funcoes/10-verificar-numero-maior.js
@@ -1,3 +1,14 @@
 /**
  * 10. Crie uma função que receba dois números e retorne o maior deles.
  */
+
+function retornarNumeroMaior(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return "São iguais!";
+  }
+}
+console.log(retornarNumeroMaior(10, 10));
--- exercicios/funcoes/9-verificar-se-palindromo.js
+++ exercicios/funcoes/9-verificar-se-palindromo.js
@@ -1,3 +1,14 @@
 /**
  * 9. Crie uma função que receba uma palavra e retorne true se ela for um palíndromo, ou false caso contrário.
  */
+function verificarSePalindromo(palavra = "") {
+  let listaDeLetras = palavra.split("");
+  let listaDeLetrasReversas = listaDeLetras.reverse();
+  let palavraReversa = listaDeLetrasReversas.join("");
+  let ePalindromo = palavra === palavraReversa;
+  return ePalindromo;
+}
+
+let resultado = verificarSePalindromo("LAYANE");
+
+console.log(`É palidromo? ${resultado}`);
