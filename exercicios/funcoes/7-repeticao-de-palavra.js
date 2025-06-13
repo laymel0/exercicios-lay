461a05589e157b541cef5634757e667de11f2ddc
laymel0 <antonia_melo_pereira@hotmail.com>
2025-06-12 21:52:36 -0300

    respostas dos exercicios

--- exercicios/funcoes/7-repeticao-de-palavra.js
+++ exercicios/funcoes/7-repeticao-de-palavra.js
@@ -1,3 +1,15 @@
 /**
  * 7. Crie uma função que receba uma palavra e um número, e retorne essa palavra repetida N vezes. 
  */
+function repeatWord( word , number) {
+    console.log ("\n Quem é o menino mais lindo desse mundo? \n")
+    for (let i = 0; i < number; i++ ){
+        console.log ("♥" + word+ "♥");
+    }
+     
+}
+repeatWord("Lucas é o menino mais lindo do mundo", 10 );
+
+
+
+ 
--- exercicios/funcoes/8-fatorial-de-um-numero.js
+++ exercicios/funcoes/8-fatorial-de-um-numero.js
@@ -1,3 +1,25 @@
 /**
  * 8. Crie uma função que receba um número inteiro positivo e retorne seu fatorial.
  */
+
+function calculate(num) {
+  let result = 1;
+  for (let i = num; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
+}
+const result1 = calculate(4);
+const result2 = calculate(5);
+const result3 = calculate(6);
+const result4 = calculate(7);
+const result5 = calculate(0);
+const result6 = calculate(1);
+
+console.log(
+  `O resultado do fatorial de: \n ${result1} \n ${result2} \n ${result3} \n ${result4} \n ${result5} \n ${result6}`
+);
+
+
+
+
