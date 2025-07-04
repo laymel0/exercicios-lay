6b5b5688f8e8bad3d696ee80f4a8de852174d90b
lucas.conceicao <lucas.conceicao@dbserver.com.br>
2025-07-03 22:28:02 -0300

    manipulacao de array parte 1

---
+++ aulas/manipulacao-de-array.js
@@ -0,0 +1,8 @@
+const listaDeComprasSegunda = ['Arroz', 'Feijao', 'Oleo', 'Macarrao', 'Cebola', 'Alho'];
+const listaDeComprasTerca = ['Carne', 'Frango', 'Peixe', 'Ovo'];
+
+// console.log(listaDeComprasSegunda.at(2)) -> Retorna o valor de um item da lista no index;
+// console.log(listaDeComprasSegunda.concat(listaDeComprasSegunda, listaDeComprasTerca)); -> Junta duas ou mais listas em uma única;
+// console.log(listaDeComprasSegunda.every((palavra: string) -> palavra.toLocaleUpperCase().includes('o'))); -> Verifica se todos os itens de uma lista atende a um critério;
+// console.log(listaDeComprasSegunda.includes('Arroz')); -> Verifica se um item está incluso na lista;
+// console.log(listaDeComprasSegunda.reverse()) -> Reverte a lista de trás pra frente;
